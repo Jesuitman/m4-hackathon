bf776b2e7cd4d48286543343374f90dfedeac80b
Arden Ranta <arranta20@gmail.com>
2024-02-16 09:31:22 -0800

    feat/add state

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,2 +1,13 @@
 import './Button.css'
+import { useState } from "react";
 
+const Button = () => {
+  const [fontSize, setFontSize] = useState(36);
+
+  return (
+    <button style={{fontSize: `${fontSize}px`}} onChange={() => setFontSize(fontSize + 20)}></button>
+  );
+
+}
+
+export default Button
