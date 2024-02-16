0b9dca11f3854d0148cebd5f0b444c4e053c974a
Arden Ranta <arranta20@gmail.com>
2024-02-16 12:32:00 -0800

    feat/add growth to all clickable elements

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,8 +1,8 @@
 import './Button.css'
 import { useState } from "react";
 
-const Button = ({type}) => {
-  const [fontSize, setFontSize] = useState(36);
+const Button = ({type, setFontSize, fontSize}) => {
+  
 
   return (
     <button type={type} style={{fontSize: `${fontSize}px`}} onClick={() => setFontSize(fontSize + 20)}>Button Text</button>
